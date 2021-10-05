1861ba0baa709cc220ea0ffd04c3a541d76ef82c
darya-rajabzadeh <daryarajabzadeh46@gmail.com>
2021-10-05 04:28:38 -0700

    personal-information

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;">
+</canvas>
+
+    <form text-align="center">
+      first name:<input type="text" id="txtfirstname" name="txtfirstname" /><br />
+      last name:<input type="text" id="txtlastname" name="txtlastname" /><br />
+      <input type="reset" value="clear" />
+      <input type="submit" value="submit" /><br /><br />
+    </form>
+   
+    <table >
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>family</th>
+          <th>action</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
     </div>
   );
 }
